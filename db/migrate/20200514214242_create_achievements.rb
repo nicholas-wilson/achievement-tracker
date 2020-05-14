735068a07c0a834bff2ba099f7192cf22a70e446
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-05-14 15:06:50 -0700

    Added User model and fixed the DB tables for users and achievements

--- db/migrate/20200514214242_create_achievements.rb
+++ db/migrate/20200514214242_create_achievements.rb
@@ -1,10 +1,10 @@
 class CreateAchievements < ActiveRecord::Migration[6.0]
   def change
     create_table :achievements do |t|
-      t.title :string
-      t.description :string
-      t.value :integer
-      t.completed :boolean
+      t.string :title
+      t.string :description
+      t.integer :value
+      t.boolean :completed
       t.timestamps
     end
   end
