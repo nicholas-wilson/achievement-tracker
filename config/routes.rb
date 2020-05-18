4229dc75e19fb0fb60912ba267fa8ccec2a01e4e
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-05-17 22:28:42 -0700

    Adding home route for login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/auth/facebook/callback', to: 'sessions#create'
+  get '/', to: "sessions#index"
 end
