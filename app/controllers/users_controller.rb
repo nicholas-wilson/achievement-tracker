cb2ba05d49d55c591b31e7341d63ede6ba5ecf84
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-05-19 17:26:51 -0700

    Added a show page for users when they create or log into their account.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,21 @@
 class UsersController < ApplicationController
   
   def new
-    @user = User.new
+    if !current_user
+      @user = User.new
+    else
+      redirect_to user_path(current_user)
+    end
   end
 
   def create
     user = User.create(user_params)
     session[:user_id] = user.id
-    redirect_to root_path
+    redirect_to user_path(user)
+  end
+
+  def show
+    @user = current_user
   end
 
   private
