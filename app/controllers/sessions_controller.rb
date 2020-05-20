32ca9be8128458b7fd7d7b09b03f3ddbee8c0c95
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-05-20 15:38:21 -0700

    Basic login structure implemented

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,18 +1,32 @@
 class SessionsController < ApplicationController
 
   def index
-    
+    if current_user
+      redirect_to user_path(current_user)
+    end
   end
 
   def new
-
+    if current_user
+      redirect_to user_path(current_user)
+    end
+    # we let rails implicitly render our view for logging in
   end
 
   def create
-
+    user = User.find_by(username: params[:username])
+    authenticated = user.try(:authenticate, params[:password])
+    if authenticated
+      session[:user_id] = user.id
+      redirect_to user_path(user)
+    else
+      # Show validation errors here and redirect to login page
+      redirect_to "/login"
+    end
   end
 
   def destroy
-
+    session.clear
+    redirect_to '/login'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/auth/facebook/callback', to: 'sessions#create'
   root to: "sessions#index"
-  resources :sessions, only: [:new, :create, :destroy]
+  get '/login', to: "sessions#new"
+  post '/login', to: "sessions#create"
+  delete '/logout', to: "sessions#destroy"
   resources :users, only: [:new, :create, :show]
 end
