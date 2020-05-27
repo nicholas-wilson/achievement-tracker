47ce294e98ee5d4fd57cefab123f67ba49d6555b
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-05-26 20:29:54 -0700

    Can now create an achievement

--- app/controllers/achievements_controller.rb
+++ app/controllers/achievements_controller.rb
@@ -1,7 +1,8 @@
 class AchievementsController < ApplicationController
 
   def index
-
+    # Only nested route for users/:id/achievements
+    # To be implemented
   end
 
   def new
@@ -10,15 +11,32 @@ class AchievementsController < ApplicationController
   end
 
   def create
-
+    # validations to be made at a later time <------- don't forget
+    if achievement = Achievement.create(achievement_params)
+      redirect_to achievement_path(achievement)
+    else
+      # Tell the user the problem with the data they gave you.
+      redirect_to achievement_new_path
+    end
   end
 
   def show
 
   end
 
+  def edit
+
+  end
+
+  def destroy
+    
+  end
+
   private
 
+  def achievement_params
+    params.require(:achievement).permit(:title, :description, :value, :completed)
+  end
   def redirect_if_not_logged_in
     if !current_user
       redirect_to '/login'
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
   resources :users, only: [:new, :create, :show]
-  resources :achievements, only: [:index, :show, :new, :create]
+  resources :achievements
 end
