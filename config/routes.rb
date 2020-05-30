8f8359c38c127c2959cb6e7aaf2cf02d94527ddc
nicholas-wilson <nicholas.wilson@nevada.unr.edu>
2020-05-29 20:10:05 -0700

    starting nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   get '/login', to: "sessions#new"
   post '/login', to: "sessions#create"
   delete '/logout', to: "sessions#destroy"
-  resources :users, only: [:new, :create, :show]
-  resources :achievements
+  resources :users, only: [:new, :create, :show] do
+    resources :achievements, only: [:index, :show]
+  end
+  resources :achievements, only: [:new, :create, :edit, :destroy]
   resources :goals, only: [:new, :create, :show, :index]
 end
